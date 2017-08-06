f6d007f3c65ec16384a3af029f01f3241d49da76
Adam Strauch <cx@initd.cz>
2017-08-06 13:58:56 +0200

    Initial commit

---
+++ remote/remote.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import socket
+import readchar
+import sys
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+ 
+UDP_IP = "192.168.1.34"
+UDP_PORT = 1234
+ 
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+
+
+def go(front_left, front_right, rear_left, rear_right):
+    cmd = "go {} {} {} {}".format(
+        front_left,
+        front_right,
+        rear_left,
+        rear_right,
+    )
+    sock.sendto(cmd.encode(), (UDP_IP, UDP_PORT))
+
+def stop():
+    sock.sendto("stop".encode(), (UDP_IP, UDP_PORT))
+
+def init():
+    sock.sendto("init".encode(), (UDP_IP, UDP_PORT))
+
+while True:
+    key = readchar.readkey()
+    if key == "q":
+        sys.exit(0)
+    elif key == "w":
+        go(100, 100, 100, 100)
+    elif key == "s":
+        print("not supported")
+    elif key == "a":
+        go(0, 100, 0, 100)
+    elif key == "d":
+        go(100, 0, 100, 0)
+    elif key == "e":
+        stop()
+    elif key == "i":
+        init()
+    else:
+        stop()
